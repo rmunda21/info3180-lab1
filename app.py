50254b295cfb4af62dc884e6092d3d5b05907140
Ricardo Munda <rjmunda21@gmail.com>
2023-01-31 11:13:43 -0500

    Added a second route and about page

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "My Home Page"
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
